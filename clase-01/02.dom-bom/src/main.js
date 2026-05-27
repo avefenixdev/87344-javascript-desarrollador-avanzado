98c7f9cacf75c483e1c615e6ede3338799ce2c27
Ave Fenix Dev <avefenixdev@gmail.com>
2026-05-27 16:38:13 -0300

    Empezando con DOM y BOM

---
+++ clase-01/02.dom-bom/src/main.js
@@ -0,0 +1,22 @@
+import './style.css'
+
+
+console.warn('// ! dom -> document object model')
+
+console.dir(document)
+
+console.warn('// ! BOM -> Browser Object Model')
+// https://developer.mozilla.org/es/docs/Web/API
+
+console.log(window)
+
+// https://3con14.biz/code/_data/js/intro/dom.png
+// https://miro.medium.com/v2/1*gRMkpPF9o_hAPGS4iJKCDQ.png
+
+// ECMAScript (Genera los estandares para que Javascript se puede utilziar en todos los navegadores y el backend de la misma manera)
+
+// https://ecma-international.org/
+
+// Javascript no es ECMAScript -> Define el lenguaje Javascripts
+// Que el navegador agregar (APIs) -> BOM -> Fetch, Geolocation, HTMLCollection
+
