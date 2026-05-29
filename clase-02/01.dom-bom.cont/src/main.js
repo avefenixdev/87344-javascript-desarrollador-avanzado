4a8afcd80dec2dcec14584ae571402d36edfa039
Ave Fenix Dev <avefenixdev@gmail.com>
2026-05-29 15:11:56 -0300

    Enunciado para trabajo con formularios

--- clase-02/01.dom-bom.cont/src/main.js
+++ clase-02/01.dom-bom.cont/src/main.js
@@ -21,5 +21,37 @@ contenedorPrincipal.appendChild(articulo)
 const cuadroDeTexto = document.querySelector('input')
 console.log(cuadroDeTexto)
 
-cuadroDeTexto.setAttribute('id', '5')
+//cuadroDeTexto.setAttribute('id', '5')
 cuadroDeTexto.classList.add('bg-green-600')
+
+const input = document.getElementById('lbl-nombre')
+console.log(input)
+
+// 1. La variable donde se guarda el dato ingresado.
+console.log(input.name)
+
+// 2. Tomar el dato ingresado en el input
+console.log(input.value)
+
+const formu = document.getElementById('formu')
+
+console.log(formu)
+
+formu.addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log(formu)
+    console.log(input.value) // aaa
+    console.log(input.name) // nombre-oficial
+    formu.reset()
+})
+
+// Enunciado. Formulario Básico
+/* 
+* Vamos crear un formulario en el html que tenga
+Nombre (texto), Email (email), edad (número) Y un botón enviar
+
+* Requisitos: No debe recargarse la página cuando el formulario es enviado
+* Válidar: Campos deben ser obligatorios. 
+* Mensaje (Extra): Si algún dato es inválido, mostrar un mensaje en patalla.
+* Mensaje (Extra): Se envío correctamente el formulario.
+*/
