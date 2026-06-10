a7c660fabb96f4d24cc6d168552c5061ee7702cc
Ave Fenix Dev <avefenixdev@gmail.com>
2026-06-10 15:30:50 -0300

    trabajando con la spa, ya hicimos la zona de template main

--- clase-07/01.single-page-application/eslint.config.js
+++ clase-07/01.single-page-application/eslint.config.js
@@ -1,14 +1,18 @@
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
 import prettierPlugin from 'eslint-plugin-prettier';
+import globals from 'globals';
 
 export default [
   js.configs.recommended,
   {
     languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: {
-        console: 'readonly',
-        process: 'readonly',
+        document: 'readonly',
+        ...globals.browser,
+        ...globals.es2021,
       },
     },
     plugins: {
--- clase-07/01.single-page-application/src/main.js
+++ clase-07/01.single-page-application/src/main.js
@@ -1,24 +1,78 @@
 // eslint-disable-next-line no-unused-vars
 import * as bootstrap from 'bootstrap'; // Agrego los scripts de bootstrap
 import './style.css';
+import ajax from './utils/ajax';
 
-const apellido = 'Principe';
-const nombre = 'Maxi';
+// ! --------------------------
+// ! Cargar barra de navegación
+// ! --------------------------
 
-console.log(nombre);
-console.log(apellido);
+async function cargarNav() {
+  console.log('Carga Navbar');
 
-console.log('Maxi');
-
-const total = 0;
-
-function sumar(a, b) {
-  const resultado = a + b;
-  return resultado;
+  try {
+    // ! 1. Selecionar la etiqueta header
+    const header = document.querySelector('header');
+    console.log(header);
+    // ! 2. Hacer petición para obtener el navbar.html
+    const template = await ajax('navbar.html');
+    //console.log(template);
+    // ! 3. Inyectar dentro del header el template
+    header.innerHTML = template;
+    // ! 4. Una vez cargado el navbar voy a cargar el resto d elas páginas
+    await getPlantillaSinHistory();
+  } catch (error) {
+    console.error('No se pudo cargar el navbar', error);
+  }
 }
 
-if (total === '0') {
-  console.log('Total es cero');
+async function getPlantillaSinHistory() {
+  console.log('GET Plantillas Sin History');
+
+  try {
+    const main = document.querySelector('.container');
+    console.log(main);
+
+    // ! ------------------------
+    // ! Cargamos el HOME -------
+    // ! ------------------------
+    const archivo = 'home.html';
+
+    const template = await ajax(archivo);
+
+    main.innerHTML = template;
+
+    // ! ------------------------------------------------------------
+    // ! Cargamos el resto de las páginas (según corresponda) -------
+    // ! ------------------------------------------------------------
+    const links = document.querySelectorAll('a');
+    console.log(links); // Lista de nodos de tipo a
+
+    links.forEach((link) => {
+      //console.log(link);
+
+      link.addEventListener('click', async () => {
+        try {
+          console.log(link.id);
+
+          const archivo = link.id + '.html';
+          console.log(archivo);
+
+          const template = await ajax(archivo);
+          main.innerHTML = template;
+        } catch (error) {
+          console.error(error);
+        }
+      });
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-sumar(2, 5);
+function start() {
+  cargarNav();
+}
+
+/* Todos los nodos html estén disponibles */
+document.addEventListener('DOMContentLoaded', start);
