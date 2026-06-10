a9c9f3ea3dd6e257be01fb9e3cf851b3bc85e4ef
Ave Fenix Dev <avefenixdev@gmail.com>
2026-06-10 14:16:32 -0300

    Empezando con la clase 07

---
+++ clase-07/01.single-page-application/src/main.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-unused-vars
+import * as bootstrap from 'bootstrap'; // Agrego los scripts de bootstrap
+import './style.css';
+
+const apellido = 'Principe';
+const nombre = 'Maxi';
+
+console.log(nombre);
+console.log(apellido);
+
+console.log('Maxi');
+
+const total = 0;
+
+function sumar(a, b) {
+  const resultado = a + b;
+  return resultado;
+}
+
+if (total === '0') {
+  console.log('Total es cero');
+}
+
+sumar(2, 5);
