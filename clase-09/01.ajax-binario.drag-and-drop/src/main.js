be5815bbe17b98259d463621e66c370a57a8edf9
Ave Fenix Dev <avefenixdev@gmail.com>
2026-06-17 15:31:47 -0300

    selección y agrego un evento al input file

--- clase-09/01.ajax-binario.drag-and-drop/src/main.js
+++ clase-09/01.ajax-binario.drag-and-drop/src/main.js
@@ -1,1 +1,11 @@
-import './style.css'
+import './style.css';
+
+// Eventos
+// Input file
+const inputFile = document.querySelector('#input-file');
+
+inputFile.addEventListener('change', (e) => {
+  console.log('Cambió el input!');
+  const archivo = e.target.files[0];
+  console.dir(archivo);
+});
