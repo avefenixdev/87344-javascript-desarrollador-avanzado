ad757a8fb25f68c4e2624258a97f2426cfb270ec
Ave Fenix Dev <avefenixdev@gmail.com>
2026-06-17 15:59:39 -0300

    Eventos de drag and drop

--- clase-09/01.ajax-binario.drag-and-drop/src/main.js
+++ clase-09/01.ajax-binario.drag-and-drop/src/main.js
@@ -9,3 +9,30 @@ inputFile.addEventListener('change', (e) => {
   const archivo = e.target.files[0];
   console.dir(archivo);
 });
+
+// Drag and Drop
+const dropArea = document.querySelector('#drop-area');
+// https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
+
+dropArea.addEventListener('dragover', (e) => {
+  e.preventDefault();
+  console.log('dragover');
+});
+dropArea.addEventListener('dragleave', (e) => {
+  e.preventDefault();
+  console.log('dragleave');
+});
+dropArea.addEventListener('drop', (e) => {
+  e.preventDefault();
+  console.log('drop');
+});
+
+document.addEventListener('dragover', (e) => {
+  e.preventDefault();
+});
+document.addEventListener('dragleave', (e) => {
+  e.preventDefault();
+});
+document.addEventListener('drop', (e) => {
+  e.preventDefault();
+});
