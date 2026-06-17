a8ec528bd52cbb56ff7ab11f6c9bcb62a3af1f3c
Ave Fenix Dev <avefenixdev@gmail.com>
2026-06-17 16:19:10 -0300

    Listo handleFile

--- clase-09/01.ajax-binario.drag-and-drop/src/main.js
+++ clase-09/01.ajax-binario.drag-and-drop/src/main.js
@@ -1,5 +1,19 @@
 import './style.css';
 
+// Función para gestionar el archivo recibido
+
+const handleFile = (file) => {
+  console.log(file);
+  const reader = new FileReader();
+  reader.readAsDataURL(file); // Lee el archivo como una URL
+  reader.onload = () => {
+    //console.log(reader.result);
+    const preview = document.querySelector('#preview');
+    preview.src = reader.result;
+    preview.classList.remove('hidden');
+  };
+};
+
 // Eventos
 // Input file
 const inputFile = document.querySelector('#input-file');
@@ -7,13 +21,15 @@ const inputFile = document.querySelector('#input-file');
 inputFile.addEventListener('change', (e) => {
   console.log('Cambió el input!');
   const archivo = e.target.files[0];
-  console.dir(archivo);
+  // console.dir(archivo);
+  handleFile(archivo);
 });
 
 // Drag and Drop
 const dropArea = document.querySelector('#drop-area');
 // https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
-
+// https://developer.mozilla.org/es/docs/Web/API/File
+// https://developer.mozilla.org/es/docs/Web/API/FileReader
 dropArea.addEventListener('dragover', (e) => {
   e.preventDefault();
   console.log('dragover');
@@ -25,8 +41,12 @@ dropArea.addEventListener('dragleave', (e) => {
 dropArea.addEventListener('drop', (e) => {
   e.preventDefault();
   console.log('drop');
+  // console.log(e.dataTransfer.files[0]);
+  const archivo = e.dataTransfer.files[0];
+  handleFile(archivo);
 });
 
+// Cancelo el comportamiento de abrir el recurso si estoy fuera de la zona de drag & drop
 document.addEventListener('dragover', (e) => {
   e.preventDefault();
 });
